cde1b6f074c6bcd6fd3007caec6a59bf59a3d43e
myrteza-labi <myrteza@hotmail.fr>
2024-09-27 19:20:32 +0200

    wip

--- api/permissions.py
+++ api/permissions.py
@@ -2,19 +2,12 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 from .models import Contributor
 
 class IsAuthorOrReadOnly(BasePermission):
-    """
-    L'auteur peut modifier ou supprimer la ressource.
-    Les autres utilisateurs ne peuvent que la lire.
-    """
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         return obj.author_user == request.user
 
 class IsContributor(BasePermission):
-    """
-    Vérifie si l'utilisateur est contributeur du projet.
-    """
     def has_permission(self, request, view):
         project_id = view.kwargs.get('project_pk') or request.data.get('project')
         if project_id:
